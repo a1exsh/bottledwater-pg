4bf3f9b045d318783c7418401f157a27e0a7f4f0
Oleksandr Shulgin <oleksandr.shulgin@zalando.de>
2015-06-12 18:48:26 +0200

    Add handling of multiple export jobs in bottledwater.py
    Uses select.select() to switch on the open connections.  It should be
    also possible to use multiprocessing/multithreads externally instead.

--- client/bottledwater.py
+++ client/bottledwater.py
@@ -1,13 +1,24 @@
 import psycopg2
+import select
 
-def get_replication_slot_restart_lsn(curs, slot_name):
-    curs.execute("SELECT restart_lsn FROM pg_replication_slots WHERE slot_name = %s",
-                 (slot_name,))
-    res = curs.fetchone()
-    if res:
-        res = res[0]
-    curs.connection.commit()
-    return res
+
+def wait_for_connections(conns):
+    while True:
+        (rd, wr, xc) = ([], [], [])
+
+        for c in conns:
+            state = c.poll()
+            if state == psycopg2.extensions.POLL_OK:
+                return c
+            elif state == psycopg2.extensions.POLL_READ:
+                rd.append(c)
+            elif state == psycopg2.extensions.POLL_WRITE:
+                wr.append(c)
+            else:
+                raise psycopg2.OperationalError("poll() returned %s" % state)
+
+        select.select(rd, wr, xc)
+
 
 def get_export_table_list(curs):
     curs.execute("""
@@ -20,21 +31,93 @@ SELECT c.relname, n.nspname
    AND n.nspname NOT LIKE 'pg_%%' AND n.nspname <> 'information_schema'
    AND c.relpersistence = 'p'
 """)
+    wait_for_connections([curs.connection])
     return curs.fetchall()
 
-def export_snapshot(curs, snapshot_name, writer):
-    curs.connection.set_isolation_level(psycopg2.extensions.ISOLATION_LEVEL_REPEATABLE_READ)
+
+def get_replication_slot_restart_lsn(curs, slot_name):
+    curs.execute("SELECT restart_lsn FROM pg_replication_slots WHERE slot_name = %s",
+                 (slot_name,))
+    wait_for_connections([curs.connection])
+    res = curs.fetchone()
+    if res:
+        return res[0]
+
+
+def begin_snapshot_transaction(curs, snapshot_name):
+    curs.execute("BEGIN TRANSACTION ISOLATION LEVEL REPEATABLE READ")
+    wait_for_connections([curs.connection])
+
     curs.execute("SET TRANSACTION SNAPSHOT %s", (snapshot_name,))
+    wait_for_connections([curs.connection])
 
-    tables = get_export_table_list(curs)
 
-    # TODO: can be done in parallel
-    for tbl in tables:
-        curs.execute("SELECT * FROM bottledwater_export_json(%s, %s)", tbl) # (name, schema)
-        for row in curs:
-            writer.write(psycopg2.extras.ReplicationMessage(row[0]))
+def export_snapshot(master_curs, snapshot_name, writer, snapshot_policy, max_jobs=1):
+    master_conn = master_curs.connection
 
-    curs.connection.commit()
+    begin_snapshot_transaction(master_curs, snapshot_name)
+    tables = get_export_table_list(master_curs)
+
+    #
+    # We keep all open connections, including the master one in two
+    # dicts: idle and active.  The mapping is from a connection to an
+    # open cursor.
+    #
+    # We start with one idle connection: the master one.
+    #
+    idle = {}
+    idle[master_conn] = master_curs
+    active = {}
+
+    # loop while we have more tables or some active connections
+    while tables or active:
+        #
+        # Check if we still have tables to export, and there are some
+        # idle connections (or we haven't maxed out the jobs yet).
+        #
+        while tables and (idle or len(active) < max_jobs):
+
+            tbl = tables.pop(0)
+            if snapshot_policy(*tbl):
+                print("next table: %s" % repr(tbl))
+
+                if idle:
+                    print("using an idle connection")
+                    (conn, curs) = idle.popitem()
+                    active[conn] = curs
+                else:
+                    print("making a new connection (%d/%d max.)" % (len(active)+1, max_jobs))
+                    conn = psycopg2.connect(master_conn.dsn, async=True)
+                    wait_for_connections([conn])
+                    curs = conn.cursor()
+                    begin_snapshot_transaction(curs, snapshot_name)
+                    active[conn] = curs
+
+                # now we have conn and curs objects: actually run the
+                # export statement
+                curs.execute("SELECT * FROM bottledwater_export_json(%s, %s)", tbl) # tuple
+
+        # wait for any of the active connections
+        conn = wait_for_connections(active.keys())
+        curs = active[conn]
+
+        row = curs.fetchone()
+        if row:
+            writer.write(psycopg2.extras.ReplicationMessage(curs, row[0]))
+        else:
+            active.pop(conn)
+            idle[conn] = curs
+
+    idle.pop(master_conn)
+
+    # all connections should be idle at this point: close them
+    for conn in idle:
+        conn.close()
+    #master_conn.commit()
+
+
+def policy_export_all(relname, relnamespace):
+    return True
 
 #
 # Example of writer class for the export:
@@ -53,7 +136,9 @@ def export_snapshot(curs, snapshot_name, writer):
 # writer = SampleBottledwaterWriter()
 # bottledwater.export(writer, dsn, slot, options={'format': 'json'})
 #
-def export(writer, dsn, slot_name, options=None, create_slot=False, initial_snapshot=False):
+def export(writer, dsn, slot_name, options=None, create_slot=False,
+           initial_snapshot=False, snapshot_policy=policy_export_all,
+           max_snapshot_jobs=1):
     from psycopg2.extras import ReplicationConnection, REPLICATION_LOGICAL
 
     #
@@ -61,7 +146,8 @@ def export(writer, dsn, slot_name, options=None, create_slot=False, initial_snap
     # if replication slot exists, and export if not) and the other one
     # for streaming.
     #
-    conn = psycopg2.connect(dsn)
+    conn = psycopg2.connect(dsn, async=True)
+    wait_for_connections([conn])
     curs = conn.cursor()
 
     replconn = psycopg2.connect(dsn, connection_factory=ReplicationConnection)
@@ -70,9 +156,15 @@ def export(writer, dsn, slot_name, options=None, create_slot=False, initial_snap
     restart_lsn = get_replication_slot_restart_lsn(curs, slot_name)
     if restart_lsn:
         print("Found existing replication slot.")
+        if initial_snapshot:
+            print("Cannot make snapshot from existing replication slot.")
+            exit(1)
     else:
         print("Replication slot doesn't exist.")
+
         if create_slot:
+            print("Creating logical replication slot %s for bottledwater..." % slot_name)
+
             replcurs.create_replication_slot(REPLICATION_LOGICAL, slot_name, "bottledwater")
             (name, restart_lsn, snapshot_name, plugin_name) = replcurs.fetchone()
         else:
@@ -80,8 +172,15 @@ def export(writer, dsn, slot_name, options=None, create_slot=False, initial_snap
             return
 
         if initial_snapshot:
-            print("Exporting tables using snapshot %s %s" % (snapshot_name, restart_lsn))
-            export_snapshot(curs, snapshot_name, writer)
+            print("Exporting tables using snapshot %s %s (max. connections: %d)" %
+                  (snapshot_name, restart_lsn, max_snapshot_jobs))
+
+            try:
+                export_snapshot(curs, snapshot_name, writer, snapshot_policy,
+                                max_jobs=max_snapshot_jobs)
+            except:
+                replcurs.drop_replication_slot(slot_name)
+                raise
 
     # we no longer need the regular connection
     conn.close()
@@ -91,4 +190,3 @@ def export(writer, dsn, slot_name, options=None, create_slot=False, initial_snap
                                slot_name=slot_name,
                                start_lsn=restart_lsn,
                                options=options)
-    replconn.close()
